b22e8eba44737352e561057061bf7e1fa7cb1dcf
Alex Stuckey <alex@stuckey.org.uk>
2017-03-11 22:19:43 +0000

    Fixing global variables

--- webapp/moodbox.js
+++ webapp/moodbox.js
@@ -2,14 +2,11 @@
 
 window.data = {};
 window.playing = true;
-
-var queue = [];
+window.theQueue = [];
 
 $(document).ready(function(){
 
   requestUpdate();
-
-  changeTrack(queue[0].title, queue[0].artwork, queue[0].length, queue[0].artist);
 
   setInterval(function(){requestUpdate()}, 1000);
   console.log("doc ready");
@@ -23,19 +20,25 @@ function requestUpdate()
   var jqxhr = $.getJSON('http://drop.robbie.xyz/fbmoodbox/dummyjson.php',function(d){
 
     console.log(d);
-    data = d;
-
-    queue = data.queue;
+    handleData(d);
+    changeTrack(window.theQueue[0].title, window.theQueue[0].artwork, window.theQueue[0].length, window.theQueue[0].artist);
 
   }).fail(function(e){
     console.log("error (probably invalid JSON)",e)});
 }
 
+function handleData(d)
+{
+  window.theQueue = d.queue;
+}
+
 function requestNextSong()
 {
+  window.theQueue.shift();
+  changeTrack(window.theQueue[0].title, window.theQueue[0].artwork, window.theQueue[0].length, window.theQueue[0].artist);
   // http://localhost/api.php?action=nextsong
   var jqxhr = $.getJSON('http://localhost/api.php?action=nextsong');
-  changeTrack(data.queue[1].title, data.queue[1].artwork, data.queue[1].length, data.queue[1].artist);
+  
 }
 
 function requestPause()
