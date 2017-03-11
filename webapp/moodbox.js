236559faa8c4467e678158d196be36131980d53d
Robbie Jakob-Whitworth <robzwolf@gmail.com>
2017-03-11 18:19:35 +0000

    Added ID to artwork img tag. Started parsing JSON.

--- webapp/moodbox.js
+++ webapp/moodbox.js
@@ -5,11 +5,27 @@ $(document).ready(function(){
   setTimeout(function(){
     // http://localhost/api.php?action=update
     var jqxhr = $.getJSON('http://drop.robbie.xyz/fbmoodbox/dummyjson.json',function(data){
+      // console.log("success");
+
+      //var data = JSON.parse(e.responseText);
+
       console.log(data);
+
+      // If data is an empty object
+      if(data.length == 0)
+      {
+
+      }
+      else
+      {
+        $("#track_name").text(data[0].title)
+      }
+
     })
     .fail(function(e){
-      console.log(e.responseText);
-      console.log(JSON.parse(e.responseText));
+      //console.log(e.responseText);
+
+
     })
 
   }, 1000);
