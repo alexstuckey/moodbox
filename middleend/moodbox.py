b2bf6fa984572f3a3c4004d8f90d1f5f290bee7c
Vivek Mehta <vivek@vivek-mehta.com>
2017-03-12 08:44:58 +0000

    Final Changes

--- middleend/moodbox.py
+++ middleend/moodbox.py
@@ -6,9 +6,8 @@ import operator
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import requests
 import json
-import urllib.request
+import urllib2
 
 # # Api Script
 # _url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
@@ -341,7 +340,10 @@ while cont == False:
         try:
             getJSONForMoodRequest = 'http://community.dur.ac.uk/mohammed.m.rahman/moodbox/backend/api.php?action=getTrack&emotion=' + str(
                 playlist)
-            getJSONForMoodData = urllib.request.urlopen(getJSONForMoodRequest)
+            getJSONForMoodData = urllib2.Request(getJSONForMoodRequest)
+            response = urllib2.urlopen(getJSONForMoodData)
+            output = response.read().split(",")
+
             # print(getJSONForMoodData.read())
         except ValueError:  # includes simplejson.decoder.JSONDecodeError
             print('Decoding JSON has failed')
@@ -350,9 +352,7 @@ while cont == False:
         cont = True
     time.sleep(10)
 
-output = getJSONForMoodData.read().decode('utf-8').replace(" ", "").split(",")
-
-#print(output)
+##print(output)
 os.system("echo clear | nc -U /home/pi/vlc.sock")
 for song in output:
     # songs.append(song['stream_url'])
@@ -366,17 +366,19 @@ os.system("echo goto 1 | nc -U /home/pi/vlc.sock")
 ##os.system("echo next | nc -U /home/pi/vlc.sock")
 #os.system("echo previous | nc -U /home/pi/vlc.sock")
 
-
 while True:
     try:
         getControlRequst = 'http://community.dur.ac.uk/mohammed.m.rahman/moodbox/backend/api.php?action=updatePi'
-        getControlRequstData = urllib.request.urlopen(getControlRequst).read().decode("utf-8")
+        getControlRequstData = urllib2.Request(getControlRequst)
+        getControlRequstData = urllib2.urlopen(getControlRequst)
+        getControlRequstData = getControlRequstData.read()
+
+        
     except ValueError:
         print('Decoding JSON has failed')
 
 
- #   print(getControlRequstData)
-    getControlRequstData = str(getControlRequstData)
+##    print(getControlRequstData)
 
     if getControlRequstData == 'next':
         os.system("echo next | nc -U /home/pi/vlc.sock")
